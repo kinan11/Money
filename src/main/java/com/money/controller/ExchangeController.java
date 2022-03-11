393931f19b78e8090f02467b31776af6c3cdd6d2
kinan11 <kingamiszczak1@op.pl>
2022-03-11 12:15:18 +0100

    Final commit

--- src/main/java/com/money/controller/ExchangeController.java
+++ src/main/java/com/money/controller/ExchangeController.java
@@ -1,31 +1,11 @@
 package com.money.controller;
 import com.money.models.Exchange;
-import com.money.models.Gold;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -53,7 +33,6 @@ public class ExchangeController {
                 e[i]=new Exchange(matcher.group(2),Double.parseDouble(matcher.group(3)));
                 i++;
             }
-
         }catch (Exception ex)
         {
             System.out.println("Error");
